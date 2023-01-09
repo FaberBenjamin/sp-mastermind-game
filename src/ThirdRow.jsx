ebe89a63638f48d4304e47f6d91e6859cbde8f11
FaberBenjamin <117324010+FaberBenjamin@users.noreply.github.com>
2023-01-09 14:47:30 +0100

    Update ThirdRow.jsx

--- src/ThirdRow.jsx
+++ src/ThirdRow.jsx
@@ -76,6 +76,7 @@ const ThirdRow = (props) => {
 
   return (
     <div id="row-3" class="color-row">
+    <div className="color_container">
       <div
         onClick={playerChoseColorHandler}
         ref={currentColorFirst}
@@ -101,6 +102,7 @@ const ThirdRow = (props) => {
         {blackDivs}
         {basicDivs}
       </div>
+</div>
       <button
         onClick={submitHandler}
         className={
